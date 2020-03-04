8f68ecc1c361e87f2f50d4e67ebbd72f6080fc5e
Daniel <danielkvist@protonmail.com>
2020-03-04 16:49:29 +0100

    update Index component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react"
 
 import Hero from "../components/hero/Hero"
+import IndexContent from "../components/indexContent/IndexContent"
 
 const IndexPage = () => {
   return (
     <React.Fragment>
-      <Hero />
+      <main>
+        <Hero />
+        <IndexContent />
+      </main>
     </React.Fragment>
   )
 }
