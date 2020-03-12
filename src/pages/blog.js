883582f6ec1be760acbf9cecd4bc1b54f5239fae
Daniel <danielkvist@protonmail.com>
2020-03-12 23:37:04 +0100

    add blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/layout/Layout';
+import Head from '../components/head/Head';
+
+const BlogPage = () => {
+  return (
+    <Layout>
+      <Head title="Blog" />
+    </Layout>
+  );
+};
+
+export default BlogPage;
